f0386ab5b6efd53829ccaed9497bd812d218987d
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-03-09 18:04:32 -0600

    -Added YAMLRegistry

---
+++ src/com/windhoverlabs/studio/YAMLRegistry.java
@@ -0,0 +1,14 @@
+package com.windhoverlabs.studio;
+
+import java.util.LinkedHashMap;
+
+
+public class YAMLRegistry implements ConfigRegistry {
+
+	@Override
+	public LinkedHashMap<?, ?> get(String registryPath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
